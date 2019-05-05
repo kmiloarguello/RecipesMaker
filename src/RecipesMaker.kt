782d03bb82d622277f6b124ae224b758d68f806a
Camilo Arguello Cencosud <camiloandres.arguellorincon@cencosud.com.co>
2019-05-05 15:24:07 -0500

    Creación de receta 1

--- src/RecipesMaker.kt
+++ src/RecipesMaker.kt
@@ -1,8 +1,9 @@
 import java.util.Scanner
 import kotlin.system.exitProcess
 
-
 val ingredients = listOf("Agua", "Leche", "Carne", "Verduras", "Frutas", "Cereal","Huevos", "Aceite")
+val recipes = listOf<String>()
+val recipe = mutableListOf<String>()
 
 fun main(args: Array<String>){
     launchMenu()
@@ -38,10 +39,36 @@ fun getUserValue ():Int{
     return reader.nextInt()
 }
 
-fun createNewRecipe (){
+fun getIngredientValue(index: Int): String {
+    val reader = Scanner(System.`in`)
+    if(reader.nextInt().equals(1)){
+        return ingredients[index]
+    }else{
+        return "no-recipe"
+    }
+}
 
+fun createNewRecipe (){
+    println("Vas a crear una nueva receta, por favor indica los ingredientes que necesitas, usa los números de tu teclado.")
+    for((index,ingredient) in ingredients.withIndex()){
+        println("Necesitarás $ingredient ? \n 0. No | 1. Si")
+        addIngredientToNewRecipe(getIngredientValue(index))
+    }
+
+
+
+    //getUserValue()
+}
+
+
+
+fun addIngredientToNewRecipe (ingredient: String) {
+    if(ingredient != "no-recipe"){
+        recipe.add(ingredient)
+        println("Recipe is: $recipe")
+    }
 }
 
 fun getAllTheRecipes () {
-
+    println("This will bring back all the recipes")
 }
