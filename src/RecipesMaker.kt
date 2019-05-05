335f45950043e303469a3dcf971ba0957cd4a1be
Camilo Arguello Cencosud <camiloandres.arguellorincon@cencosud.com.co>
2019-05-05 16:13:06 -0500

    Finalización de receta

--- src/RecipesMaker.kt
+++ src/RecipesMaker.kt
@@ -2,15 +2,19 @@ import java.util.Scanner
 import kotlin.system.exitProcess
 
 val ingredients = listOf("Agua", "Leche", "Carne", "Verduras", "Frutas", "Cereal","Huevos", "Aceite")
-val recipes = listOf<String>()
-val recipe = mutableListOf<String>()
+val recipe1 = mutableListOf<String>()
+val recipe2 = mutableListOf<String>()
+val recipe3 = mutableListOf<String>()
+val recipes = mutableListOf<MutableList<String>>()
+val names = mutableListOf<String>()
 
 fun main(args: Array<String>){
+    println("::Bienvenido a Recipe Maker::\n\n")
+
     launchMenu()
 }
 
 fun launchMenu(){
-    println("::Bienvenido a Recipe Maker::\n\n")
 
     val menuText = """
         Selecciona la opción deseada
@@ -28,7 +32,7 @@ fun launchMenu(){
 
 fun decideUserSelection (userSelection: Int){
     when(userSelection){
-        1 -> createNewRecipe()
+        1 -> createNewRecipe(recipe1)
         2 -> getAllTheRecipes()
         3 -> exitProcess(0)
     }
@@ -48,27 +52,61 @@ fun getIngredientValue(index: Int): String {
     }
 }
 
-fun createNewRecipe (){
+fun createNewRecipe (recipe: MutableList<String>){
+
     println("Vas a crear una nueva receta, por favor indica los ingredientes que necesitas, usa los números de tu teclado.")
     for((index,ingredient) in ingredients.withIndex()){
         println("Necesitarás $ingredient ? \n 0. No | 1. Si")
-        addIngredientToNewRecipe(getIngredientValue(index))
+        addIngredientToNewRecipe(getIngredientValue(index), recipe)
+
+        if(index == ingredients.size.minus(1)){
+            println("Le quieres poner un nombre ?");
+            val recipeName = readLine()!!
+
+            getCurrentRecipe(recipeName, recipe)
+            askToCreateAnotherRecipe()
+
+
+            // Total recipes
+            recipes.add(recipe)
+            names.add(recipeName)
+        }
+
     }
-
-
-
-    //getUserValue()
 }
 
 
-
-fun addIngredientToNewRecipe (ingredient: String) {
+fun addIngredientToNewRecipe (ingredient: String, recipe: MutableList<String>) {
     if(ingredient != "no-recipe"){
         recipe.add(ingredient)
-        println("Recipe is: $recipe")
+    }
+}
+
+fun getCurrentRecipe(name:String, recipe: List<String>){
+    println("La receta actual es $name con los ingredientes:")
+    for(ingredient in recipe){
+        println(ingredient)
+    }
+}
+
+fun askToCreateAnotherRecipe(){
+    println("¿Deseas crear otra receta? 0. No | 1. SI")
+    val reader = Scanner(System.`in`)
+    if(reader.nextInt().equals(1)){
+        createNewRecipe(recipe2) //TODO: Hacer más de 2 recetas
+    } else {
+        launchMenu()
     }
 }
 
 fun getAllTheRecipes () {
-    println("This will bring back all the recipes")
+    for(recipe in recipes){
+        for(ingredient in recipe){
+            println("Ingrediente $ingredient de la receta $recipe") // Print the recipes
+
+            if(ingredient.equals(recipe.minus(1))){
+                askToCreateAnotherRecipe()
+            }
+        }
+    }
 }
